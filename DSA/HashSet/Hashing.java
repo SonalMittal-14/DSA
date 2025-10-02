fed2f4587ddf6acd7b58a13be3ef4c09bde99106
SonalMittal-14 <sonalmittal140103@gmail.com>
2025-10-02 16:32:53 +0530

    HashSet

---
+++ DSA/HashSet/Hashing.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+public class Hashing{
+    public static void main(String[] args) {
+        // Craetion of Hashset
+        HashSet<Integer> set = new HashSet<>();
+
+        // Insertion
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        System.out.println("size of set is : "+ set.size());
+
+        // printing all elements of set
+        System.out.println("all elements of set are : "+ set);
+        
+        // searching
+        if(set.contains(1)){
+            System.out.println("set contain 1.");
+        }
+        if(!set.contains(6)){
+            System.out.println("does not contains 6");
+        }
+        
+        // deletion
+        set.remove(1);
+        if(!set.contains(1)){
+            System.out.println("set does not contains 1 .... coz we deleted it ");
+        }
+        
+        System.out.println("size of set after deletion is : "+ set.size());
+        System.out.println("all elements of set after deletion  are : "+ set);
+    }
+}

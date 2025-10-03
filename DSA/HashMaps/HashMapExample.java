dd9d8dc3063e530d5aaf1a6edb7325c5964e0f9a
SonalMittal-14 <sonalmittal140103@gmail.com>
2025-10-03 17:45:40 +0530

    HashMAp

---
+++ DSA/HashMaps/HashMapExample.java
@@ -0,0 +1,16 @@
+package HashMaps;
+import java.util.*;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        // country (key), population (value)
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // insertion
+        map.put("India", 120);
+        map.put("USA", 30);
+        map.put("China", 150);
+
+        System.out.println(map);
+    }
+}

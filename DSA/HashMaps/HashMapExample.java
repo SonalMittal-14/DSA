425c4633e1c2bfa3c493926f74b60b0181de0544
SonalMittal-14 <sonalmittal140103@gmail.com>
2025-10-06 11:37:19 +0530

    HashMaps

--- DSA/HashMaps/HashMapExample.java
+++ DSA/HashMaps/HashMapExample.java
@@ -15,5 +15,49 @@ public class HashMapExample {
 
         map.put("China", 200);
         System.out.println(map);
+
+
+        if(map.containsKey("america")){
+            System.out.println("key is present in the map");
+        }else{
+            System.out.println("key is not present in the map");
+        }
+
+        System.out.println(map.get("India"));
+        System.out.println(map.get("america")); // does not exist 
+
+
+
+        // Iteration in HashMap
+        int arr[] = {12,5,18};
+        for(int i=0; i<3; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+
+        for(int val : arr){
+            System.out.print(val + " ");
+        }
+
+        System.out.println();
+
+
+
+        for(Map.Entry<String , Integer> e : map.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+
+        Set<String> keys = map.keySet();
+        for(String key : keys){
+            System.out.println(key + " " + map.get(key));
+        }
+
+
+        // Removing key from the HashMap
+        map.remove("China");
+        System.out.println(map);
     }
 }
